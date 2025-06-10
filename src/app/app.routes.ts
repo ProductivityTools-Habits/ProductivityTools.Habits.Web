627c54258e4557f4efeb955edf8370d853a67705
Pawel Wujczyk <pwujczyk@gmail.com>
2025-06-10 18:55:44 +0200

    habit component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HabitList } from './habits/habit-list/habit-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:HabitList }
+];
