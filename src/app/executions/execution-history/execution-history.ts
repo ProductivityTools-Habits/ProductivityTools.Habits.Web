675d6e635999edc0e8b62d2328ae040a93a8485b
Pawel Wujczyk <pwujczyk@gmail.com>
2025-10-04 00:26:53 +0200

    execution-history

--- src/app/executions/execution-history/execution-history.ts
+++ src/app/executions/execution-history/execution-history.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ExecutionService } from '../executions.service';
+import { HabitsService } from '../../habits/habits.service';
+import { combineLatest, Subscription } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-execution-history',
@@ -6,6 +11,35 @@ import { Component } from '@angular/core';
   templateUrl: './execution-history.html',
   styleUrl: './execution-history.css'
 })
-export class ExecutionHistory {
+export class ExecutionHistory implements OnInit, OnDestroy {
 
+  private subscription: Subscription = new Subscription();
+
+  constructor(private executionService: ExecutionService, private habitsService: HabitsService) {
+
+  }
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  loadData(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    this.subscription = new Subscription();
+
+    const executions$=this.executionService.getExecutionsObservable();
+    const habits$=this.habitsService.getHabitsObservable();
+
+    this.subscription.add(
+      combineLatest([habits$,executions$]).subscribe(data=>{
+        console.log("executions history data:",data);
+      })
+    )
+  }
 }
